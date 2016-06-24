a2561d20c02eb4f2a62a6e7cbb8b8a5e420ba003
Matt Sheen <mws262@cornell.edu>
2016-06-24 14:05:45 -0400

    cleaned up files, added gitignore

--- backup/ClickDown.m
+++
@@ -1,7 +0,0 @@
-function ClickDown(varargin)
-global f Fx Fy
-Fx = 0;
-Fy = 0;
-
-    set(f,'WindowButtonMotionFcn',@MousePos);
-end
--- backup/ClickUp.m
+++
@@ -1,6 +0,0 @@
-function ClickUp(varargin)
-global f Fx Fy
-    set(f,'WindowButtonMotionFcn','');
-    Fx = 0;
-    Fy = 0;
-end
